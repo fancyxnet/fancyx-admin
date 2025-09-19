fee2e6025233fd63064519db1223be9fc9cb04da
cracker <fancyxnet@gmail.com>
2025-09-19 18:00:31 +0800

    fix:删除冗余包,代码

--- fancyx-server/src/Fancyx.DataAccess/FancyxAdminEfCoreModule.cs
+++ fancyx-server/src/Fancyx.DataAccess/FancyxAdminEfCoreModule.cs
@@ -1,25 +1,16 @@
 ﻿using Fancyx.Core.AutoInject;
 using Fancyx.Core.Context;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Fancyx.EfCore;
 
 namespace Fancyx.Admin.EfCore
 {
+    [DependsOn(
+        typeof(FancyxEfCoreModule)
+        )]
     public class FancyxAdminEfCoreModule : ModuleBase
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            context.Services.AddDbContext<DbContext, FancyxDbContext>((sp, options) =>
-            {
-                options.UseNpgsql(context.Configuration.GetConnectionString("Default"))
-                     .LogTo(Console.WriteLine, LogLevel.Information)
-#if DEBUG
-                     .EnableSensitiveDataLogging()
-#endif
-                     .EnableDetailedErrors();
-            }, ServiceLifetime.Scoped);
         }
 
         public override void Configure(ApplicationInitializationContext context)
