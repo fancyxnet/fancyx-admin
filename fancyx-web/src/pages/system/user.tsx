54417452b06212da639500560ed4dd60b16d4fe1
cracker <fancyxnet@gmail.com>
2025-12-11 14:42:56 +0800

    user table

--- fancyx-web/src/pages/system/user.tsx
+++ fancyx-web/src/pages/system/user.tsx
@@ -1,4 +1,4 @@
-import { Button, Switch, Space, Form, Input, Avatar, Col, Card, List, Row, Tag, Dropdown } from 'antd';
+import { Button, Switch, Space, Avatar, Col, Card, List, Row, Tag, Dropdown } from 'antd';
 import { useEffect, useRef, useState } from 'react';
 import {
   DeleteOutlined,
@@ -18,8 +18,6 @@ import {
 } from '@/api/system/user';
 import UserEditForm, { type ModalRef } from '@/pages/system/components/UserForm.tsx';
 import AssignRoleForm, { type AssignRoleFormRef } from '@/pages/system/components/AssignRoleForm.tsx';
-import SmartTable from '@/components/SmartTable';
-import type { SmartTableRef, SmartTableColumnType } from '@/components/SmartTable/type.ts';
 import ResetUserPwdForm, { type ResetUserPwdFormRef } from '@/pages/system/components/ResetUserPwdForm.tsx';
 import ProIcon from '@/components/ProIcon';
 import { useApplication } from '@/components/Application';
@@ -28,26 +26,29 @@ import { getDeptSimpleInfos, type DeptSimpleInfo } from '@/api/organization/dept
 import Search from 'antd/es/input/Search';
 import './styles/user.scss';
 import { useAuthProvider } from '@/components/AuthProvider';
+import { ProTable, type ActionType, type ProColumnType } from '@ant-design/pro-components';
 
-const UserTable = () => {
-  const tableRef = useRef<SmartTableRef>(null);
+const User: React.FC = () => {
+  const actionRef = useRef<ActionType>();
   const { message, modal } = useApp();
   const userEditModalRef = useRef<ModalRef>(null);
   const assignRoleRef = useRef<AssignRoleFormRef>(null);
   const resetUserPwdFormRef = useRef<ResetUserPwdFormRef>(null);
   const { ossDomain } = useApplication();
   const { hasPermission } = useAuthProvider();
-  const columns: SmartTableColumnType[] = [
+  const columns: ProColumnType<UserItem>[] = [
     {
       key: 'index',
       render: (_: any, __: any, index: number) => index + 1,
+      search: false,
     },
     {
       title: '头像',
       dataIndex: 'avatar',
-      render: (text: string) => {
-        return <Avatar size={50} src={ossDomain + text} />;
+      render: (_: any, record: UserItem) => {
+        return <Avatar size={50} src={ossDomain + record.avatar} />;
       },
+      search: false,
     },
     {
       title: '账号',
@@ -57,20 +58,23 @@ const UserTable = () => {
     {
       title: '昵称',
       dataIndex: 'nickName',
+      search: false,
     },
     {
       title: '性别',
       dataIndex: 'sex',
       key: 'sex',
-      render: (text: number) => (text === 1 ? '男' : '女'),
+      render: (_: any, record: UserItem) => (record.sex === 1 ? '男' : '女'),
+      search: false,
     },
     {
       title: '状态',
       dataIndex: 'isEnabled',
       key: 'isEnabled',
-      render: (text: boolean, record: UserItem) => (
+      search: false,
+      render: (_: any, record: UserItem) => (
         <Switch
-          checked={text}
+          checked={record.isEnabled}
           checkedChildren="启用"
           unCheckedChildren="禁用"
           onChange={() => onUserStatusChange(record)}
@@ -80,20 +84,24 @@ const UserTable = () => {
     {
       title: '手机号',
       dataIndex: 'phone',
+      search: false,
     },
     {
       title: '部门',
       dataIndex: 'deptName',
+      search: false,
     },
     {
       title: '岗位',
       dataIndex: 'postName',
+      search: false,
     },
     {
       title: '操作',
       key: 'action',
       width: 180,
       fixed: 'right',
+      search: false,
       render: (_: any, record: UserItem) => {
         const curDropdownItems = [];
         if (hasPermission!('Sys.User.AssignRole')) {
@@ -109,7 +117,7 @@ const UserTable = () => {
                 分配角色
               </a>
             ),
-            onClick: () => {},
+            onClick: () => { },
           });
         }
         if (hasPermission!('Sys.User.ResetPwd')) {
@@ -125,7 +133,7 @@ const UserTable = () => {
                 重置密码
               </a>
             ),
-            onClick: () => {},
+            onClick: () => { },
           });
         }
         return (
@@ -187,7 +195,7 @@ const UserTable = () => {
     setCurDept(null);
   };
   useEffect(() => {
-    tableRef?.current?.reload();
+    actionRef?.current?.reload();
   }, [curDept?.id]);
 
   const rowDelete = (id: string) => {
@@ -197,7 +205,7 @@ const UserTable = () => {
       onOk() {
         deleteUser(id).then(() => {
           message.success('删除成功');
-          tableRef?.current?.reload();
+          actionRef?.current?.reload();
         });
       },
     });
@@ -205,56 +213,57 @@ const UserTable = () => {
   const onUserStatusChange = (record: UserItem) => {
     switchUserEnabledStatus(record.id).then(() => {
       message.success('状态更改成功');
-      tableRef?.current?.reload();
+      actionRef?.current?.reload();
     });
   };
 
-  return (
-    <>
-      <Row gutter={16}>
-        <Col span={6}>
-          <Card>
-            {curDept?.id?.length && curDept?.id?.length > 0 && (
-              <Tag closeIcon onClose={preventDefault} style={{ marginBottom: 12 }}>
-                筛选部门：{curDept?.name}
-              </Tag>
+  return (<div className='fancyx-table-wrapper'>
+    <Row gutter={16}>
+      <Col span={6}>
+        <Card>
+          {curDept?.id?.length && curDept?.id?.length > 0 && (
+            <Tag closeIcon onClose={preventDefault} style={{ marginBottom: 12 }}>
+              筛选部门：{curDept?.name}
+            </Tag>
+          )}
+          <Search placeholder="请输入部门名称/编码" style={{ marginBottom: 12 }} allowClear onSearch={onDeptSearch} />
+          <List
+            loading={deptLoading}
+            size="small"
+            dataSource={deptData}
+            className="dept-list"
+            renderItem={(item) => (
+              <List.Item>
+                <span
+                  className="dept-text"
+                  onClick={() => {
+                    setCurDept(item);
+                  }}
+                >
+                  {item.name}({item.code})
+                </span>
+              </List.Item>
             )}
-            <Search placeholder="请输入部门名称/编码" style={{ marginBottom: 12 }} allowClear onSearch={onDeptSearch} />
-            <List
-              loading={deptLoading}
-              size="small"
-              dataSource={deptData}
-              className="dept-list"
-              renderItem={(item) => (
-                <List.Item>
-                  <span
-                    className="dept-text"
-                    onClick={() => {
-                      setCurDept(item);
-                    }}
-                  >
-                    {item.name}({item.code})
-                  </span>
-                </List.Item>
-              )}
-            />
-          </Card>
-        </Col>
-        <Col span={18}>
-          <SmartTable
-            rowKey="id"
-            columns={columns}
-            ref={tableRef}
-            request={async (params) => {
-              const { data } = await getUserList({ ...params, deptId: curDept?.id });
-              return data;
-            }}
-            searchItems={
-              <Form.Item<GetUserListRequest> label="账号" name="userName">
-                <Input placeholder="请输入账号" />
-              </Form.Item>
-            }
-            toolbar={
+          />
+        </Card>
+      </Col>
+      <Col span={18}>
+        <ProTable<UserItem, GetUserListRequest>
+          actionRef={actionRef}
+          rowKey="id"
+          columns={columns}
+          request={async (
+            params: GetUserListRequest
+          ) => {
+            const res = await getUserList(params);
+            return {
+              data: res.data.items,
+              success: true,
+              total: res.data.totalCount
+            };
+          }}
+          toolBarRender={
+            () => [
               <Permission permissions={'Sys.User.Add'}>
                 <Button
                   color="primary"
@@ -267,17 +276,18 @@ const UserTable = () => {
                   新增
                 </Button>
               </Permission>
-            }
-          />
-        </Col>
-      </Row>
-      {/* 新增/编辑弹窗 */}
-      <UserEditForm ref={userEditModalRef} refresh={() => tableRef?.current?.reload()} />
-      {/* 分配角色弹窗 */}
-      <AssignRoleForm ref={assignRoleRef} />
-      {/* 重置密码弹窗 */}
-      <ResetUserPwdForm ref={resetUserPwdFormRef} />
-    </>
-  );
-};
-export default UserTable;
+            ]
+          }
+        />
+      </Col>
+    </Row>
+    {/* 新增/编辑弹窗 */}
+    <UserEditForm ref={userEditModalRef} refresh={() => actionRef?.current?.reload()} />
+    {/* 分配角色弹窗 */}
+    <AssignRoleForm ref={assignRoleRef} />
+    {/* 重置密码弹窗 */}
+    <ResetUserPwdForm ref={resetUserPwdFormRef} />
+  </div>)
+}
+
+export default User;
