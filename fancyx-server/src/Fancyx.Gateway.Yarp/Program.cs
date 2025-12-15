6bf6ecfec5ef69f8935d8f2e42fa508b36084215
cracker <fancyxnet@gmail.com>
2025-12-15 10:29:36 +0800

    yarp读取consul配置

--- fancyx-server/src/Fancyx.Gateway.Yarp/Program.cs
+++ fancyx-server/src/Fancyx.Gateway.Yarp/Program.cs
@@ -1,4 +1,12 @@
+using Fancyx.Consul;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var isUseConsul = builder.Configuration["ServiceMode"] == "Consul";
+if (isUseConsul)
+{
+    builder.Services.AddConsulSetup(builder.Configuration);
+}
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
@@ -19,6 +27,15 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+if (isUseConsul)
+{
+    ConsulRegistration.Register(app.Services);
+    app.Map(ConsulConstant.ConsulHealthUrl, () =>
+    {
+        return Results.Ok("yarp is ok");
+    });
+}
 
 app.UseCors();
 app.MapReverseProxy();
